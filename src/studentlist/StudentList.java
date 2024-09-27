234c74924deb8f87716020a718995de238d48e54
Jasleen kaur <kau17610@sheridancollege.ca>
2024-09-27 15:42:38 -0400

    hello

--- src/studentlist/StudentList.java
+++ src/studentlist/StudentList.java
@@ -14,7 +14,7 @@ public class StudentList {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String[] students = {"jasleen","taran","jassi","Manpreet","navleeen","sahil"};
+        String[] students = {"jasleen","taran","jassi","Manpreet","navleeen"};
         for (String i:students){
             System.out.println(i);
         }}
